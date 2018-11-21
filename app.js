3cf861dd20f26d7ff622a5e7d5b23e8159aa5d56
Tim Bui <buitim@oregonstate.edu>
2018-11-20 23:51:47 -0800

    ...

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 var express = require('express');
 var app = express();
-app.get('/', function (req, res) {
+
+// set the port of our application
+// process.env.PORT lets the port be set by Heroku
+var port = process.env.PORT || 8080;
+
+// make express look in the public directory for assets (css/js/img)
+app.use(express.static(__dirname + '/public'));
+
+app.get('/test', function (req, res) {
 	res.send('Hello World!');
 });
-var server = app.listen(5000, function () {
+
+var server = app.listen(port, function () {
 	console.log(`== Express running → PORT ${server.address().port}`);
 });
