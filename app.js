ff4cf2e0c445e843983460a1682cd6b6bd5c6b10
Tim Bui <buitim@oregonstate.edu>
2018-11-21 02:13:14 -0800

    Started ejs templating (if we actually need it)

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var app = express();
 
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
 // set the port of our application
 // process.env.PORT lets the port be set by Heroku
 var port = process.env.PORT || 8080;
@@ -8,8 +11,8 @@ var port = process.env.PORT || 8080;
 // make express look in the public directory for assets (css/js/img)
 app.use(express.static(__dirname + '/public'));
 
-app.get('/test', function (req, res) {
-	res.send('Hello World!');
+app.get('/app', function (req, res) {
+	res.render('index');
 });
 
 var server = app.listen(port, function () {
