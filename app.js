4df13bc641a01a1ce1439a8d056cddb7e9b5c625
jeffdeng1314 <dengh@oregonstate.edu>
2018-11-27 04:45:20 -0800

    Create works, check first switch (progress) lmao

--- app.js
+++ app.js
@@ -208,81 +208,88 @@ function build_browse_query(parameters) {
 }
 
 
-function process_create_parameters(parameters){
+// checks query parameters and returns a list of valid values
+function process_read_parameters(parameters) {
 
-	var year, name, character_first, character_last, voice_first, voice_last, season, studio;
+	var type;
 
-	// console.log(parameters);
+	console.log(parameters);
 
-	// if ('title' in parameters){
-	// 	if(parameters.title )
+	if ('type' in parameters) {
 
-	// }
-	// if('year')
-	// year = parameters.year;
-	// name = parameters.title;
-	// character_first = parameters.
-	for(x in parameters){
-		if(parameters[x] == '\0')
-			parameters[x] = NULL;
+		var type_valid = compare_values(parameters.type, ['show', 'character', 'voice_actor', 'studio']);
+		if (type_valid === true) {
+			type = parameters.type;
+		} else {
+			type = 'show';
+		}
+
+	} else {
+		type = 'show';
 	}
 
-	year = parameters.year;
-	name = parameters.show;
-	character_first = parameters.char_first;
-	character_last = parameters.char_last;
-	voice_first = parameters.voice_first;
-	voice_last = parameters.voice_last;
-	studio = parameters.studio;
-	season = parameters.season;
+	console.log(type);
 
-	return [name,character_first,character_last,voice_first,voice_last,studio,year,season];
-
+	return [ type ];
 
 }
 
-function build_create_query(parameters) {
+// constructs query for database using array of strings as parameters
+function build_read_query(parameters) {
 
-	var show_query, season_query, character_query, studio_query, voice_query;
-	var query1, query2, query3, query4, query5;
+	var query_text, query;
 
-	show_query = 'INSERT INTO public.show (title) VALUES ($1)';
-	character_query = 'INSERT INTO public.character (first_name,last_name) VALUES ($1, $2)';
-	voice_query = 'INSERT INTO public.voice_actor (first_name,last_name) VALUES ($1, $2)';
-	studio_query = 'INSERT INTO public.studio (name) VALUES ($1)';
-	season_query = 'INSERT INTO public.season (year,season) VALUES ($1, $2)';
+	query_text = 'SELECT * FROM public.' + parameters[0];
 
-	var query_array = [show_query, character_query, voice_query, studio_query, season_query];
+	query = { text: query_text };
 
-	query1 = {
-		text: show_query,
-		values: [ parameters[0] ]
-	};
-	query2 = {
-		text: character_query,
-		values: [ parameters[1], parameters[2] ]
-	};
-	query3 = {
-		text: voice_query,
-		values: [ parameters[3], parameters[4] ]
-	};
-	query4 = {
-		text: studio_query,
-		values: [ parameters[5] ]
-	};
-	query5 = {
-		text: season_query,
-		values: [ parameters[6], parameters[7] ]
-	};
-
-	var query_array = [ query1, query2, query3, query4, query5 ];
-
-	// console.log("query1: "+ query_values[0].values);
-
-	return query_array;
+	return query;
 
 }
 
+
+function build_create_query (parameters) {
+
+	var query, query_text, query_values = [];
+	var sequence; // value used for specifying auto increment in postgresql insert
+
+	switch(parameters.table){
+		case 'character':
+			query_text = 'INSERT INTO public.' + parameters.table + ' (first_name, last_name, actor_id, show_id) VALUES ( $1, $2, $3, $4 )';
+			query_values = [ parameters.first_name, parameters.last_name, parameters.actor_id, parameters.show_id ];
+			break;
+
+		case 'voice_actor':
+			query_text = 'INSERT INTO public.' + parameters.table + ' (actor_id, first_name, last_name) VALUES ( $1, $2, $3 )';
+			sequence = 'nextval(' + parameters.table + '_actor_id_seq)';
+			query_values = [ sequence, parameters.first_name, parameters.last_name, parameters.actor_id, parameters.show_id ];
+			break;
+
+		case 'show':
+			console.log("show");
+
+			console.log(parameters);
+			break;
+		case 'studio':
+			console.log("studio");
+
+			console.log(parameters);
+			break;
+		case 'season':
+			console.log("season");
+
+			console.log(parameters);
+			break;
+		default:
+			console.log('something weird happened');
+
+	}
+
+	query = { text: query_text, values: query_values };
+
+	return query;
+
+}
 
 // checks input from POST request form and returns list of valid parameters for delete operation on database
 function process_delete_parameters(parameters) {
@@ -400,7 +407,7 @@ app.get('/browse*', function (req, res, next) {
 	db.query(query, function (error, result) {
 
 		if (error) {
-			throw error;
+			console.log("Error!");
 		} else {
 
 			console.log("Query done!");
@@ -440,44 +447,66 @@ app.get('/create', function(req,res){
 	res.render('create');
 });
 
+
+app.get('/read', function(req,res){
+
+	var parameters = process_read_parameters(req.query);
+
+	var query = build_read_query(parameters); // this should be safe from injection, because of parameter processing 
+
+	console.log(query);
+
+	db.query(query, function (error, result) {
+
+		if (error) {
+			console.log("Error!");
+		} 
+		else {
+
+			console.log("Query done!");
+
+			var query_results = [], query_headers = [];
+
+			query_headers = underscores_to_spaces(Object.keys(result.rows[0]));
+
+			for (x in result.rows) {
+				query_results.push(Object.values(result.rows[x]));
+			}
+
+			var context = {
+				headers: query_headers,
+				results: query_results
+			};
+
+			res.render('read', context);
+
+		}
+
+	});
+
+
+});
+
 // if page for deletiing rows in the database is requested, find "delete.handlebars" in views and show it to user
 app.post('/create', function (req, res) {
 
-console.log(req.body);
+	var query = build_create_query(req.body);
+	var context;
 
-	try{
-		var parameters = process_create_parameters(req.body);
-		var query = build_create(parameters);
-	
-		// console.log("query 0: " + query[0]);
-	
-		console.log("inside the try but outside the loop");
-		for (x in query) {
-			console.log("Before query");
+	console.log(query);
 
+	db.query(query, function (error, result) {
 
-			db.query(query[x].text,query[x].values, function (error, result) {
-			
-				console.log("inside query loop");
-				if (error) {
-					throw error;
-				} 
-			});
-		}
-		res.render('create');
-	}
-	catch(e){
-	console.log("Hello!");
-	res.status(404).send("Error! boi!");
+		if (error) {
+			context = { message: error.message }
+			res.render('create', context);
+			return;
+		} 
 
-	}
+		res.render('create');	
 
-	// res.end(JSON.stringify(req.body.year));
-	// process_create_parameters(req.body.show);
-
-	// var show_name = req.body.show;
-	// console.log(show_name);
-
+	});
+		
 });
 
 
